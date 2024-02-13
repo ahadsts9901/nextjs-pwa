8905ae0272af92c592fda3b67eec492df6b4641f
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2024-02-13 13:56:45 +0500

    code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="w-full h-[100vh] p-4 bg-[#efefef] text-zinc-800 font-extrabold flex justify-center items-center">
-      <h1 className="w-full text-center text-[3rem]">Nextjs & PWA</h1>
+      <h1 className="w-full text-center text-[3rem]">Welcome User!</h1>
+      <h1 className="w-full text-center text-[2rem]">Nextjs & PWA</h1>
     </div>
   );
 }
